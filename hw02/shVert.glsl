128609687cdb276570d7409f614688f6139916f3
skyeong0810 <skyeong0810@gmail.com>
2025-03-23 16:17:13 +0900

    Delete hw02 directory

--- hw02/shVert.glsl
+++
@@ -1,8 +0,0 @@
-#version 300 es
-
-layout (location = 0) in vec3 aPos;
-uniform vec2 uOffset; //uniform variable for movement
-
-void main() {
-    gl_Position = vec4(aPos.xy + uOffset, aPos.z, 1.0);
-}
